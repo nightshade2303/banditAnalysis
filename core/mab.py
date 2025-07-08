859006e81184294f7b94dcc70210a5998379ac3f
nightshade2303 <rs.rishika.sharma99@gmail.com>
2025-07-08 19:52:34 +0530

    got from bapun's repo

--- core/mab.py
+++ core/mab.py
@@ -1,2 +1,27 @@
-# do something here...
-# new stuff here
+class Bandit4Arm(BanditTask):
+    """
+    A class to hold a multi-armed bandit task data.
+    """
+
+    def __init__(
+        self,
+        probs,
+        choices,
+        rewards,
+        session_ids,
+        starts=None,
+        stops=None,
+        datetime=None,
+        metadata=None,
+    ):
+        super().__init__(
+            probs=probs,
+            choices=choices,
+            rewards=rewards,
+            session_ids=session_ids,
+            starts=starts,
+            stops=stops,
+            datetime=datetime,
+            metadata=metadata,
+        )
+        assert self.n_ports == 4, "Bandit4Arm requires exactly 4 arms"
